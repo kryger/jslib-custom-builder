6babeb57f2508135e2508234ca99ad65d51f783a
Lukasz Kryger <l.kryger@ed.ac.uk>
2013-03-13 10:57:53 +0000

    Working ExtJS parser/trimmer, produces (initially verified) correct output.

---
+++ libraries/extjs41/block_finder.py
@@ -0,0 +1,36 @@
+import re, os
+
+class ExtBlockFinder:
+  EXT_DEFINE_START = "Ext.define('%s'"
+  EXT_DEFINE_END = '});'
+
+  def __init__(self, content_lines):
+      self.content_lines = content_lines
+
+  def __get_start_index(self, class_name):
+    """Return first line of the class definition, return -1 if class not found."""    
+    for index, line in enumerate(self.content_lines):
+      if line.startswith(self.EXT_DEFINE_START % class_name):
+        return index
+
+    return -1
+
+  def __get_end_index(self, start_index, class_name):
+    """Return numer of the line that ends the specified class definition."""
+    for line_index in range(start_index, len(self.content_lines)):
+      line = self.content_lines[line_index]
+      if line.startswith(self.EXT_DEFINE_END):
+        return line_index
+    
+    raise Exception('Unable to match end for class %s' % class_name)
+      
+  def find(self, class_name):
+    """Return a tuple containing index of the first and last line of the class definition.""" 
+     
+    start = self.__get_start_index(class_name)
+    if start == -1:
+      print 'WARNING, class name=%s not found, probably incorrectly extracted' % class_name
+      return (0, 0)
+      
+    end = self.__get_end_index(start, class_name)
+    return (start, end)
--- main.py
+++ main.py
@@ -1,86 +1,8 @@
-import argparse, os, re
-from Queue import Queue
-from libraries.extjs41.parse_class import ExtClassParser
-EXT_CREATE_TOKEN = 'Ext.create\((.*?)\)'
+import argparse
+from libraries.extjs41.build_trimmer import BuildTrimmer
+from libraries.extjs41.usage_parser import ExtBuilder
 
-class ExtBuilder:
-  def __init__(self, js_project_path, ext_path):
-    self.js_project_path = js_project_path
-    self.ext_path = ext_path
-     
-   
-  def build(self):
-    ext_hierarchy = self.__create_ext_class_hierarchy()
-    
-    modules_used = self.__extract_used_modules()
-    already_processed = set()
-    
-    q = Queue()
-    for module in modules_used:
-      q.put(module)
-      
-      
-    while not q.empty():
-      module = q.get()
-      
-      parser = ExtClassParser(self.ext_path, module)
-      already_processed.add(module)
-      
-      dependencies = parser.get_requires()
-      parent = parser.get_extends()
-      if parent and (parent not in already_processed):
-        q.put(parser.get_extends())
-        
-      for dependency in dependencies:
-        if dependency not in already_processed:
-          q.put(dependency)
-          
-    return already_processed
-      
-  def __create_ext_class_hierarchy(self):
-    for (dirpath, dirnames, filenames) in os.walk(self.ext_path):
-        for filename in filenames:
-          ext_class_file = '%s/%s' % (dirpath, filename)
-          
-          #with open(ext_class_file, 'r') as f:
-           #   contents = f.read();
-          p = ExtClassParser(self.ext_path, ext_class_file)
-          
-          
-          
-              #matches =  re.search(EXT_CREATE_TOKEN,contents,re.DOTALL)
-#              if matches:
- #               for match in matches.groups():
-                    # at this point match contains XXX in Ext.create(XXX)
-  #                  ext_component_quoted = match.split(',')[0].strip()
-                    # add, getting rid of quotes
-   #                 ext_components.add(ext_component_quoted[1:-1])
-
-    print 'FINISHED PARSING EXTJS SOURCE'
-    return ext_components
-    
-    
-  def __extract_used_modules(self):
-    ext_components = set()
-    for (dirpath, dirnames, filenames) in os.walk(self.js_project_path):
-        for filename in filenames:
-          jsfile = '%s/%s' % (dirpath, filename)
-          
-          with open(jsfile, 'r') as f:
-              contents = f.read();
-              matches =  re.search(EXT_CREATE_TOKEN,contents,re.DOTALL)
-              if matches:
-                for match in matches.groups():
-                    # at this point match contains XXX in Ext.create(XXX)
-                    ext_component_quoted = match.split(',')[0].strip()
-                    # add, getting rid of quotes
-                    ext_components.add(ext_component_quoted[1:-1])
-
-    return ext_components
-         
-   
-   
-
+EXT_BUILD_FILE_IN = r'ext-all-debug'
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(description='jslib custom builder')
@@ -89,8 +11,18 @@ if __name__ == "__main__":
     arg_parser.add_argument('-extdir', help='Path to the ExtJS distribution folder', action='store',
                             dest='ext_dir', required=True)
     params = vars(arg_parser.parse_args())
+    ext_builder = ExtBuilder(params['source_dir'], params['ext_dir'], EXT_BUILD_FILE_IN)
     
-    ext_builder = ExtBuilder(params['source_dir'], params['ext_dir'])
-    print ext_builder.build()
-    
-    
+    need_these = [
+      'Ext.picker.Date',
+      # 'Ext.dom.Element',
+      # 'Ext.EventObject' is actually called Ext.EventObjectImpl in the 'define' block
+      'Ext.EventObject',
+      'Ext.EventObjectImpl',
+      'Ext.dom.CompositeElement',
+      'Ext.util.TaskRunner',
+      ]    
+    unused_classes = ext_builder.get_unused_classes(need_these)
+    print '\n\nunused=', unused_classes
+    b = BuildTrimmer(unused_classes, params['ext_dir'] + '/%s.js' % EXT_BUILD_FILE_IN)
+    b.trim()
